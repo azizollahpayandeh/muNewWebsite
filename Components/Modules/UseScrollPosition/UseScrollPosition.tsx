4dfdce7c188ad384d63a63a297620aa07e6968e2
azizollah <azizollahpayandeh1@gmail.com>
2023-12-15 16:57:33 +0330

    finish

--- Components/Modules/UseScrollPosition/UseScrollPosition.tsx
+++ Components/Modules/UseScrollPosition/UseScrollPosition.tsx
@@ -1,18 +1,20 @@
-"use client"
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
 export const UseScrollPosition = () => {
-    const [scrollPosition, setScrollPosition] = useState(0)
+    const [scrollPosition, setScrollPosition] = useState(0);
 
     useEffect(() => {
-        const updatePosition = () =>{
-            setScrollPosition(window.scrollY)
+        // Check if window is defined (so we're on the browser)
+        if (typeof window !== 'undefined') {
+            const updatePosition = () => {
+                setScrollPosition(window.scrollY);
+            };
+
+            window.addEventListener('scroll', updatePosition);
+            updatePosition(); // Initialize state with current scroll position
+            return () => window.removeEventListener('scroll', updatePosition);
         }
+    }, []);
 
-        window.addEventListener("scroll", updatePosition)
-        updatePosition()
-        return() => window.removeEventListener("scroll", updatePosition)
-    }, [])
-
-    return scrollPosition
-}
+    return scrollPosition;
+};
