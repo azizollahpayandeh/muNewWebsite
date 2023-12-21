d69b92a509b01292303209d0406845663be7aac5
azizollah <azizollahpayandeh1@gmail.com>
2023-12-21 21:55:11 +0330

    finsih

--- Components/Templates/NavBar/NavBar.tsx
+++ Components/Templates/NavBar/NavBar.tsx
@@ -1,14 +1,14 @@
 "use client"
 import ToolTipButtons from "@/Components/Modules/TooltipButtons/ToolTipButtons";
 import React from "react";
-// import { UseScrollPosition } from "@/Components/Modules/UseScrollPosition/UseScrollPosition";
+import { UseScrollPosition } from "@/Components/Modules/UseScrollPosition/UseScrollPosition";
 import Image from "next/image";
 
 export default function NavBar() {
-    // const scrollPosition = UseScrollPosition()
+    const scrollPosition = UseScrollPosition()
   return (
     <>
-    <div className={`w-full top-0 z-50 transition-shadow sticky h-[100px] text-white   `}>
+    <div className={`w-full top-0 z-50 transition-shadow sticky h-[100px] text-white  ${scrollPosition > 0 ? " bg-opacity-95 bg-[#151516]" : "shadow-none"}  `}>
       <div className=" pt-[15px] items-center gap-2 flex justify-between ">
         <div className="pl-[3vw]">
           <Image alt="Logo" src="/Images/logo3.png" width={70} height={70}/>
